761d67bbb38293ee56274fe69b119f77120c33ab
aditi1397 <aditi131997@gmail.com>
2017-05-26 12:52:15 -0700

    Before UserCertificateInfo

--- mysite/certificates/models.py
+++ mysite/certificates/models.py
@@ -6,35 +6,44 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user=models.OneToOneField(User)
-    name=models.CharField(max_length=75)
-    email=models.CharField(max_length=75)
-    DOB=models.DateField()
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=75)
+    email = models.CharField(max_length=75)
+    DOB = models.DateField()
 
 
 class UserType(models.Model):
-    name=models.CharField(max_length=75)
-    user=models.ManyToManyField(User)
+    name = models.CharField(max_length=75)
+    user = models.ManyToManyField(User)
 
     def __str__(self):
         return self.name
 
 
 class Certificate(models.Model):
-    latex_template=models.CharField(max_length=100)
-    logo=models.CharField(max_length=25)
+    latex_template = models.CharField(max_length=100)
+    logo = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.latex_template
 
 
-class Programs(models.Model):
-    name=models.CharField(max_length=100)
-    certificate=models.OneToOneField(Certificate,on_delete=models.CASCADE)
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    certificate = models.OneToOneField(Certificate,on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self):
+        return self.name
 
 
-class OrganisePrograms(models.Model):
-    program=models.ForeignKey(Programs)
-    start_date=models.DateField()
-    end_date=models.DateField()
-    num_of_days=models.IntegerField()
-    user=models.ManyToManyField(User)
-    days_attended=models.IntegerField()
-    qrCode=models.IntegerField()
+class OrganiseProgram(models.Model):
+    program = models.ForeignKey(Program)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    num_of_days = models.IntegerField()
+    user = models.ManyToManyField(User)
+    days_attended = models.IntegerField()
+    qrCode = models.IntegerField()
+
+    def __str__(self):
+        return self.program.name
