258f93c5999821dfe7346dfbe5925b31d89f210d
aditi1397 <aditi131997@gmail.com>
2017-06-13 16:11:03 -0700

    Add and view in views

--- mysite/certificates/urls.py
+++ mysite/certificates/urls.py
@@ -4,9 +4,17 @@ from django.contrib.auth.views import login, logout
 from . import views
 urlpatterns = [
     url(r'^$', views.login),
-    url(r'^home/$',views.home),
-    url(r'^login/$', login, {'template_name': 'certificates/login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'certificates/logout.html'}),
-    url(r'^template/$', views.add_certificate),
-    url(r'^event/$', views.add_event),
+    url(r'^home/$', views.home),
+    url(r'^home/add_user/$', views.add_user),
+    url(r'^home/edit_user/$', views.edit_user),
+    url(r'^home/view_user/$', views.view_user),
+    url(r'^home/add_certificate/$', views.add_certificate),
+    url(r'^home/edit_certificate/$', views.edit_certificate),
+    url(r'^home/view_certificate/$', views.view_certificate),
+    url(r'^home/add_event/$', views.add_event),
+    url(r'^home/edit_event/$', views.edit_event),
+    url(r'^home/view_event/$', views.view_event),
+    url(r'^home/organise_event/$', views.organise_event),
+    url(r'^home/edit_organised_event/$', views.edit_organised_event),
+    url(r'^home/view_organised_event/$', views.view_organised_event),
 ]
--- mysite/certificates/views.py
+++ mysite/certificates/views.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
-from django.contrib.auth.decorators import login_required
+from . import functions
 from django.contrib.auth import authenticate
 
 
@@ -14,8 +14,6 @@ def login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            #model_instance = form.save(commit=False)
-            #model_instance.save()
             user = authenticate(username=form['username'].value(), password=form['password'].value())
             if user is not None:
                 return redirect('home/')
@@ -26,19 +24,90 @@ def login(request):
         return render(request, 'certificates/login.html', {'form': form})
 
 
-def add_certificate(request):
+def add_user(request):
     if request.method == "POST":
-        form = CertificateForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             model_instance = form.save(commit=False)
             model_instance.save()
             return redirect('/')
     else:
+        form = UserForm()
+        return render(request, "certificates/add_modelform.html", {'form': form})
+
+
+def edit_user(request):
+    return render(request, "certificates/add_modelform.html")
+
+
+def view_user(request):
+    user = User.objects.all()
+    context = {"object_list":user}
+    return render(request, 'certificates/view_user.html', context)
+
+
+def add_certificate(request):
+    if request.method == "POST":
+        form = CertificateForm(request.POST, request.FILES)
+        if form.is_valid():
+            functions.add_certificate(request.FILES['template'], form['title'].value())
+            return redirect('/')
+    else:
         form = CertificateForm()
-        return render(request, "certificates/my_template.html", {'form': form})
+        return render(request, "certificates/add_modelform.html", {'form': form})
+
+
+def edit_certificate(request):
+    return render(request, 'certificates/edit_certificate.html')
+
+
+def view_certificate(request):
+    certificate = Certificate.objects.all()
+    context = {"object_list": certificate}
+    return render(request, 'certificates/view_certificate.html', context)
 
 
 def add_event(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.save()
+            create_event(form['name'].value(), form['certificate'].value(), form['creator'].value())
+            return redirect('/')
+    else:
+        form = EventForm()
+        return render(request, "certificates/add_modelform.html", {'form': form})
+
+
+def edit_event(request):
+    return render(request, "certificates/add_modelform.html")
+
+
+def view_event(request):
     event=Event.objects.all()
     context = {"object_list": event}
-    return render(request, 'certificates/event.html', context)
+    return render(request, 'certificates/view_event.html', context)
+
+
+def organise_event(request):
+    if request.method == "POST":
+        form = OrganisedEventForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.save()
+            return redirect('/')
+    else:
+        form = OrganisedEventForm()
+        return render(request, "certificates/add_modelform.html", {'form': form})
+
+
+def edit_organised_event(request):
+    return render(request, "certificates/add_modelform.html")
+
+
+def view_organised_event(request):
+    organised_event = OrganisedEvent.objects.all()
+    context = {"object_list": organised_event}
+    return render(request, 'certificates/view_organised_event.html', context)
+
