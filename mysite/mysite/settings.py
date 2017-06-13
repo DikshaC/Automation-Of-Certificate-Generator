d7bd8d882ab1aa0704549433632b3b455f496ff0
Diksha Chhabra <dchhabra1995@gmail.com>
2017-06-13 13:12:30 +0530

    Minor changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material',
+    'material.frontend',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'certificates',
+
 ]
 
 MIDDLEWARE = [
